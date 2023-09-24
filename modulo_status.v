ce660f9b7439ade5a3d1df21f49671d1d92f6664
Alexjr <88115079+Oguelo@users.noreply.github.com>
2023-09-23 22:54:55 -0300

    alteração modulo status

--- modulo_status.v
+++ modulo_status.v
@@ -3,7 +3,7 @@ module modulo_status(std0,stdig0);
 	input [1:0] std0;
 	output [3:0] stdig0;
 	wire [1:0] nstd0;
-	
+	wire nstd0_and_std1, std0_and_nstd1,nstd0_and_nstd1, std0_and_std1 ;
 	not(nstd0[1],std0[1]);
 	not(nstd0[0],std0[0]);
 	
@@ -12,13 +12,17 @@ module modulo_status(std0,stdig0);
 	and(stdig0[3], 1'b1,1'b1);
 	
 	//STDIG0_1;
-	and(stdig0[2],std0[1],1'b1);
-	
+	and(nstd0_and_nstd1,nstd0[0],nstd0[1]);
+	and(std0_and_std1,std0[0],std0[1]);
+	or(stdig0[2],nstd0_and_nstd1,std0_and_std1);
 	//STDIG0_2;
-	and(stdig0[1],std0[0],1'b1);
+	and(nstd0_and_std1,nstd0[0],std0[1]);
+	and(std0_and_nstd1,std0[0],nstd0[1]);
+	or(stdig0[1],nstd0_and_std1,std0_and_nstd1);
+	
 	
 	//STDIG0_3
-	and(stdig0[0],1'b0,1'b0);
+	and(stdig0[0],std0[1]);
 	
 	
 endmodule 
