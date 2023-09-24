7c884d9582232595e4dad8ce19beedf0a7f31403
Alexjr <88115079+Oguelo@users.noreply.github.com>
2023-09-23 22:21:01 -0300

    contador 3 bits

--- module modulo_contador_mod_4.v
+++ module modulo_contador_mod_4.v
@@ -1,7 +1,17 @@
-module modulo_contador_mod_4(t,clk,clr,q);
+module modulo_contador_3bits(clr, clk, q);
 
-	input t,clk,clr;
-	output reg q;
+	input clk,clr;
+	output [2:0] q;
+	wire ff1_and_ff2;
+	modulo_ff_t ff_1(.t(1'b1),.clk(clk),.clr(clr),.q(q[2]),);
 	
-
+	modulo_ff_t ff_2(.t(q[1]),.clk(clk),.clr(clr),.q(q[1]),);
+	
+	and(ff1_and_ff2, q[2],q[1]);
+	
+	
+	modulo_ff_t ff_3(.t(ff1_and_ff2),.clk(clk),.clr(clr),.q(q[0]),);
+	
+	
+	and(clr, q[2],q[1],q[0]);
 endmodule 
---
+++ module modulo_contador_mod_4.v.bak
@@ -0,0 +1,7 @@
+module modulo_contador_mod_4(t,clk,clr,q);
+
+	input t,clk,clr;
+	output reg q;
+	
+
+endmodule 
