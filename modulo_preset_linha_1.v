5989086c405cb6bf5340650ec99ec7a2d19f5ea0
BRCZ1N <brunocamposo.r@gmail.com>
2023-09-26 22:34:18 -0300

    Alteração

--- modulo_preset_linha_1.v
+++ modulo_preset_linha_1.v
@@ -7,14 +7,14 @@ module modulo_preset_linha_1 (HH,cl1);
 	not(NHH[1],HH[1]);
 	not(NHH[0],HH[0]);
 
-	and_gate_2_inputs gate_1(.A(1'b1),.B(HH[1]),.S(cl1[4]),);	
+	and_gate_2_inputs gate_1(.A(1'b1),.B(HH[0]),.S(cl1[4]),);	
 
-	and_gate_2_inputs gate_2(.A(1'b1),.B(HH[1]),.S(cl1[3]),);
+	and_gate_2_inputs gate_2(.A(1'b1),.B(HH[0]),.S(cl1[3]),);
 
-	and_gate_2_inputs gate_3(.A(HH[1]),.B(NHH[0]),.S(cl1[2]),);
+	and_gate_2_inputs gate_3(.A(HH[0]),.B(NHH[1]),.S(cl1[2]),);
 
-	or_gate_2_inputs gate_4(.A(NHH[1]),.B(NHH[0]),.S(cl1[1]),);
+	or_gate_2_inputs gate_4(.A(NHH[0]),.B(NHH[1]),.S(cl1[1]),);
 
-	and_gate_2_inputs gate_5(.A(1'b1),.B(NHH[1]),.S(cl1[0]),);
+	and_gate_2_inputs gate_5(.A(1'b1),.B(NHH[0]),.S(cl1[0]),);
 	
 endmodule 
