2da826d828ae4338984946b9daa6d11c04f47a69
BRCZ1N <brunocamposo.r@gmail.com>
2023-10-01 19:09:20 -0300

    Arrumando 1_16

--- modulo_seletor_1_16_at.v
+++ modulo_seletor_1_16_at.v
@@ -39,14 +39,18 @@ module modulo_seletor_1_16_at (mdc,mdl,dmx16_sel);
 	or_gate_3_inputs gate_14(.A(mx16e3[2]),.B(mx16e3[1]),.C(mx16e3[0]),.S(mx16e3_aux[0]),);
 	or_gate_2_inputs gate_15(.A(mx16e3_aux[1]),.B(mx16e3_aux[0]),.S(dmx16_sel[1]),);
 
+	
+	
+	
+	
 	and_gate_4_inputs gate_16(.A(nmdc[2]),.B(nmdc[1]),.C(nmdc[0]),.D(mdl[2]),.S(mx16e4[7]),);
 	and_gate_4_inputs gate_17(.A(mdc[0]),.B(nmdl[2]),.C(nmdl[1]),.D(nmdl[0]),.S(mx16e4[6]),);
-	and_gate_3_inputs gate_18(.A(mdc[1]),.B(nmdl[0]),.C(nmdl[1]),.S(mx16e4[5]),);
-	and_gate_4_inputs gate_19(.A(mdc[1]),.B(mdc[1]),.C(nmdl[2]),.D(nmdl[0]),.S(mx16e4[4]),);
+	and_gate_3_inputs gate_18(.A(mdc[1]),.B(nmdl[2]),.C(nmdl[1]),.S(mx16e4[5]),);
+	and_gate_4_inputs gate_19(.A(mdc[1]),.B(mdc[0]),.C(nmdl[2]),.D(nmdl[0]),.S(mx16e4[4]),);
 	and_gate_2_inputs gate_20(.A(mdc[2]),.B(nmdl[2]),.S(mx16e4[3]),);
 	and_gate_4_inputs gate_21(.A(nmdc[2]),.B(nmdc[1]),.C(mdl[2]),.D(mdl[0]),.S(mx16e4[2]),);
 	and_gate_4_inputs gate_22(.A(nmdc[2]),.B(nmdc[1]),.C(mdl[2]),.D(mdl[1]),.S(mx16e4[1]),);
-	and_gate_4_inputs gate_23(.A(nmdc[2]),.B(nmdc[0]),.C(nmdl[2]),.D(nmdl[1]),.S(mx16e4[0]),);
+	and_gate_4_inputs gate_23(.A(nmdc[2]),.B(nmdc[0]),.C(mdl[2]),.D(nmdl[1]),.S(mx16e4[0]),);
 	
 	or_gate_4_inputs gate_24(.A(mx16e4[7]),.B(mx16e4[6]),.C(mx16e4[5]),.D(mx16e4[4]),.S(mx16e4_aux[1]),);
 	or_gate_4_inputs gate_25(.A(mx16e4[3]),.B(mx16e4[2]),.C(mx16e4[1]),.D(mx16e4[0]),.S(mx16e4_aux[0]),);
--- modulo_seletor_1_4_at.v
+++ modulo_seletor_1_4_at.v
@@ -16,22 +16,15 @@ module modulo_seletor_1_4_at (mdc,mdl,dmx4_sel);
 	
 	
 	and_gate_3_inputs gate_1(.A(nmdc[1]),.B(nmdc[0]),.C(mdl[1]),.S(mx4e1[5]),);
-	
 	and_gate_4_inputs gate_2(.A(nmdc[1]),.B(mdc[0]),.C(nmdl[1]),.D(nmdl[0]),.S(mx4e1[4]),);
-	
 	and_gate_3_inputs gate_3(.A(nmdc[1]),.B(mdl[1]),.C(mdl[0]),.S(mx4e1[3]),);
-	
 	and_gate_3_inputs gate_4(.A(mdc[1]),.B(nmdc[0]),.C(nmdl[1]),.S(mx4e1[2]),);
-	
 	and_gate_3_inputs gate_5(.A(mdc[1]),.B(nmdl[1]),.C(mdl[0]),.S(mx4e1[1]),);
-	
 	and_gate_4_inputs gate_6(.A(mdc[1]),.B(mdc[0]),.C(mdl[1]),.D(nmdl[0]),.S(mx4e1[0]),);
 	
 	or_gate_5_inputs gate_7(.A(mx4e1[5]),.B(mx4e1[4]),.C(mx4e1[3]),.D(mx4e1[3]),.E(mx4e1[3]),.S(dmx4_sel[1]),);
 	
-	
 	and_gate_2_inputs gate_8(.A(nmdc[0]),.B(mdl[0]),.S(mx4e2[1]),);
-	
 	and_gate_2_inputs gate_9(.A(mdc[0]),.B(nmdl[0]),.S(mx4e2[0]),);
 	
 	or_gate_2_inputs gate_10(.A(mx4e2[1]),.B(mx4e2[0]),.S(dmx4_sel[0]),);
