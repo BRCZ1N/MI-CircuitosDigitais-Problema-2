81afb1192c3b64aa6acf3aa62c7319f350449708
Alexjr <88115079+Oguelo@users.noreply.github.com>
2023-10-01 23:33:22 -0300

    a

--- modulo_coord_coluna.v
+++ modulo_coord_coluna.v
@@ -1,24 +1,37 @@
 module modulo_coord_coluna(mdc,cdc);
 
-	input [2:0] mdc;
-	output [3:0] cdc;
-	wire [2:0] nmdc;
-	
-	not(nmdc[2],mdc[2]);
-	not(nmdc[1],mdc[1]);
-	not(nmdc[0],mdc[0]);
-	
-	//STDIG0_0;
-	
-	and_gate_2_inputs gate_1(.A(1'b0),.B(1'b0),.S(cdc[3]),);
-	
-	//STDIG0_1;
-	and_gate_2_inputs gate_2(.A(mdc[2]),.B(1'b1),.S(cdc[2]),);
-	
-	//STDIG0_2;
-	and_gate_2_inputs gate_3(.A(mdc[1]),.B(1'b1),.S(cdc[1]),);
-	
-	//STDIG0_3
-	and_gate_2_inputs gate_4(.A(mdc[0]),.B(1'b1),.S(cdc[0]),);
+    input [2:0] mdc;
+    output [3:0] cdc;
+    wire [2:0] nmdc;
+    wire [1:0] cdc_1_wire;
+    wire cdc_2_wire, cdc_3_wire;
 
-endmodule 
+    not(nmdc[2],mdc[2]);
+    not(nmdc[1],mdc[1]);
+    not(nmdc[0],mdc[0]);
+
+    //STDIG0_0;
+
+    and_gate_2_inputs gate_1(.A(mdc[2]),.B(mdc[0]),.S(cdc_1_wire[1]),);
+
+    and_gate_2_inputs gate_1(.A(mdc[2]),.B(mdc[1]),.S(cdc_1_wire[0]),);
+
+    or_gate_2_input gate_3(.A(cdc_1_wire[1]),.B(cdc_1_wire[0]),,.S(cdc[3]),);
+
+    //STDIG0_1;
+
+    and_gate_2_inputs gate_2(.A(mdc[2]),.B(1'b1),.S(cdc[2]),);
+
+    //STDIG0_2;
+
+    and_gate_2_inputs gate_1(.A(mdc[2]),.B(mdc[0]),.S(cdc_2_wire),);
+
+    or_gate_2_input gate_3(.A(mdc[1]),.B(cdc_2_wire),,.S(cdc[1]),);
+
+    //STDIG0_3
+
+    and_gate_2_inputs gate_1(.A(mdc[2]),.B(mdc[1]),.S(cdc_3_wire),);
+
+    or_gate_2_input gate_3(.A(mdc[0]),.B(cdc_3_wire),.S(cdc[0]),);
+
+endmodule
--- modulo_coord_linha.v
+++ modulo_coord_linha.v
@@ -1,24 +1,24 @@
 module modulo_coord_linha(mdl,cdl);
 
-	input [2:0] mdl;
-	output [3:0] cdl;
-	wire [2:0] nmdl;
-	
-	not(nmdl[2],mdl[2]);
-	not(nmdl[1],mdl[1]);
-	not(nmdl[0],mdl[0]);
-	
-	//STDIG0_0;
-	
-	and_gate_2_inputs gate_1(.A(1'b0),.B(1'b0),.S(cdl[3]),);
-	
-	//STDIG0_1;
-	and_gate_2_inputs gate_2(.A(mdl[2]),.B(1'b1),.S(cdl[2]),);
-	
-	//STDIG0_2;
-	and_gate_2_inputs gate_3(.A(mdl[1]),.B(1'b1),.S(cdl[1]),);
-	
-	//STDIG0_3
-	and_gate_2_inputs gate_4(.A(mdl[0]),.B(1'b1),.S(cdl[0]),);
+    input [2:0] mdl;
+    output [3:0] cdl;
+    wire [2:0] nmdl;
 
-endmodule 
+    not(nmdl[2],mdl[2]);
+    not(nmdl[1],mdl[1]);
+    not(nmdl[0],mdl[0]);
+
+    //STDIG0_0;
+
+    and_gate_3_inputs gate_1(.A(mdl[2]),.B(mdl[1]),.C(mdl[0]),.S(cdl[3]),);
+
+    //STDIG0_1;
+    and_gate_2_inputs gate_2(.A(mdl[2]),.B(1'b1),.S(cdl[2]),);
+
+    //STDIG0_2;
+    and_gate_2_inputs gate_3(.A(mdl[1]),.B(1'b1),.S(cdl[1]),);
+
+    //STDIG0_3
+    and_gate_2_inputs gate_4(.A(mdl[0]),.B(1'b1),.S(cdl[0]),);
+
+endmodule
